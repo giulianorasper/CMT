b8a75f5943a63f0bb383cf50e68e8f81c0512239
Giuliano Rasper <s8girasp@stud.uni-saarland.de>
2019-11-25 23:14:44 +0100

    basic JavaScript communication, usage example

---
+++ frontend/communication/CommunicationManager.js
@@ -0,0 +1,26 @@
+var timeout = 2000;
+
+export default class CommunicationManager {
+
+    static openConnection() {
+        //TODO make this configurable
+        var socket;
+        socket = new WebSocket('ws://127.0.0.1:17699');
+        return socket;
+    }
+
+    static send(packet, successHook = new function(packet){}, errorHook = new function(){}) {
+        var socket = new WebSocket('ws://127.0.0.1:17699');
+        socket.onmessage = function(event) {
+            console.log(event.data);
+            successHook(JSON.parse(event.data));
+            socket.onclose = function() {};
+            socket.close();
+        };
+        socket.onopen = () => {
+            socket.send(JSON.stringify(packet));
+        };
+        socket.onclose = errorHook;
+        socket.onerror = errorHook;
+    }
+}

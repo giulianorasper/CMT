03090cc64e0c4e8881b82e36212d5ccab240c7c0
Simon <s8sifedi@stud.uni-saarland.de>
2019-12-05 15:53:30 +0100

    several changes

--- implementation/src/test/java/TestServer.java
+++ implementation/src/test/java/TestServer.java
@@ -1,19 +1,28 @@
 import communication.CommunicationManager;
 import communication.CommunicationManagerFactory;
+import main.Conference;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class TestServer {
 
-   public static void main(String[] args) {
-        CommunicationManager w = new CommunicationManagerFactory(null).enableDebugging().create();
+    @Test
+   public void testing() throws InterruptedException{
+        CommunicationManager w = new CommunicationManagerFactory(new Conference()).enableDebugging().create();
         w.start();
-       System.out.println("Type anything to stop the serer.");
-        Scanner scanner = new Scanner(System.in);
-        scanner.next();
-        try {
-            w.stop();
-        } catch (Exception e) {
-            e.printStackTrace();
-       }
+        Thread.sleep(100000000);
    }
+
+    @Before
+    public void cleanup() {
+        URI path = Paths.get("testdb/testdb.db").toUri();
+        new File(path).delete();
+    }
 }

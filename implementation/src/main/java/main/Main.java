aa35be3d51047b98a472cf0b6759ae703f72ddba
Alexander Dincher <s8aldinc@stud.uni-saarland.de>
2020-01-14 23:16:55 +0100

    add debugging to communication again

--- implementation/src/main/java/main/Main.java
+++ implementation/src/main/java/main/Main.java
@@ -53,7 +53,7 @@ public class Main {
             conf.addAttendee(new Attendee("Peter Schmidt",  "peterschidt@t-online.de", "user", "Unicorn", "66271 Kleinblittersdorf, am Tälchen 12", "User", 2), "user");
             conf.addAttendee(new Attendee("Sabine Hein", "sahe@stud.uni-saarland.de", "SaHe", "Flower", "66181 Ensheim, Hauptstraße 12", "User", 3), "user2");
         }
-        CommunicationManager w = new CommunicationManagerFactory(conf).create();
+        CommunicationManager w = new CommunicationManagerFactory(conf).enableDebugging().create();
         w.start();
         System.out.println("Press 'q' to close the server");
         try{

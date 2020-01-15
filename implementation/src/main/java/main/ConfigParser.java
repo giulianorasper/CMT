515251a5396c073833adde5935243ce5329a7eeb
Stephan Ariesanu <s8starie@stud.uni-saarand.de>
2020-01-15 17:53:28 +0100

    added example config file and parser

---
+++ implementation/src/main/java/main/ConfigParser.java
@@ -0,0 +1,172 @@
+package main;
+
+import document.Document;
+import request.Request;
+import user.Admin;
+import voting.Voting;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ConfigParser {
+
+    static String [] validKeys = {"admin", "startTime", "endTime", "name", "organizer", "databasePath", "documentsPath"};
+
+    public static Conference parseConfigFile(String s){
+
+
+        Map<String, String> map = new HashMap<>();
+        List<List<String>> admins = new ArrayList<List<String>>();
+        AtomicInteger i = new AtomicInteger(0);
+
+        int lineNumber = 1;
+        while(i.get() < s.length()){
+            parseLine(s, i, map, admins, lineNumber, i.get());
+            lineNumber++;
+        }
+
+        for(String mandKey : new String[]{"name", "organizer", "endTime"}){
+            if(!map.containsKey(mandKey)){
+                throw new IllegalArgumentException("Missing key " +mandKey);
+            }
+        }
+
+        if(admins.isEmpty()){
+            throw new IllegalArgumentException("Having at least one admin is mandatory");
+        }
+
+        Conference conf =   new Conference
+                (  map.get("name"),
+                map.get("organizer"),
+                Long.parseLong(map.getOrDefault("startTime", ""+System.currentTimeMillis())),
+                        Long.parseLong(map.get("endTime")),
+                new HashMap<Integer, Admin>(),
+                new HashMap<Integer, Voting>(),
+                new HashMap<String, Document>(),
+                        map.getOrDefault("databasePath", "./db"),
+                new HashMap<Integer, Request>(),
+                null,
+                map.getOrDefault("documentsPath", "./docs"),
+                false,
+                false
+        );
+
+
+        admins.forEach(a ->{
+            conf.addAdmin(new Admin(a.get(0), a.get(1), conf.getFreeUserName(a.get(0)), a.get(2), a.get(3), a.get(4)));
+        });
+
+
+        return conf;
+
+    }
+
+    private static void parseLine(String s, AtomicInteger i, Map<String, String> fields, List<List<String>> admins, int lineNumber, int indexAtLineStart){
+        skipWhitespace(s,i);
+
+        if(s.charAt(i.get()) == '#'){ //ignore comments
+            parseComment(s, i);
+            skipWhitespace(s,i);
+            return;
+        }
+
+        int aux = i.get();
+        String key = parseKey(s, i, lineNumber, aux);
+        boolean contains = false;
+        for (String validKey : validKeys) {
+            if (validKey.equals(key)) {
+                contains = true;
+                break;
+            }
+        }
+        if(!contains || fields.containsKey(key) || key.isBlank()){
+            throw new IllegalArgumentException("Illegal key '"+key+"' at "+lineNumber+":"+(aux -indexAtLineStart));
+        }
+        skipWhitespace(s,i);
+
+        if(s.charAt(i.get()) != '\''){
+            throw new IllegalArgumentException("Value does not start with ''' at "+lineNumber+":"+(i.get() -indexAtLineStart) );
+        }
+        if(key.equals("admin")){
+            admins.add(parseAdmin(s, i));
+            skipWhitespace(s,i);
+            if(s.charAt(i.get()) == '#'){
+                parseComment(s, i);
+            }
+
+        }
+        else{
+            String value = parseNonAdminValue(s, i, '\'');
+            skipWhitespace(s,i);
+            if(s.charAt(i.get()) == '#'){
+                parseComment(s,i);
+            }
+
+            fields.put(key, value);
+        }
+
+    }
+
+    private static List<String> parseAdmin(String s, AtomicInteger i){
+        List<String> res= new ArrayList();
+        res.add(parseNonAdminValue(s, i, ':'));
+        res.add(parseNonAdminValue(s, i, ':'));
+        res.add(parseNonAdminValue(s, i, ':'));
+        res.add(parseNonAdminValue(s, i, ':'));
+        res.add(parseNonAdminValue(s, i, '\''));
+
+        return res;
+    }
+
+    private static String parseKey(String s, AtomicInteger i, int lineNumber, int position){
+        StringBuilder sb = new StringBuilder();
+        for(;  i.get()<s.length(); i.incrementAndGet()){
+            if(Character.isAlphabetic(s.charAt(i.get()))){
+                sb.append(s.charAt(i.get()));
+            }
+            else if(s.charAt(i.get()) == ':'){
+                i.incrementAndGet();
+                break;
+            }
+            else if(!Character.isWhitespace(s.charAt(i.get()))){
+                throw new IllegalArgumentException("Malformed key at "+lineNumber+":"+position);
+            }
+        }
+        return sb.toString().toLowerCase();
+    }
+
+    private static String parseNonAdminValue(String s, AtomicInteger i, char endAt){
+        StringBuilder sb = new StringBuilder("");
+        for(;  i.get()<s.length(); i.incrementAndGet()){
+            if(s.charAt(i.get()) == '\\'){
+                i.incrementAndGet();
+            }
+            else if(s.charAt(i.get()) == endAt){
+                return sb.toString().toLowerCase();
+            }
+            else if(s.charAt(i.get()) == '#'){
+                parseComment(s,i);
+                break;
+            }
+            sb.append(s.charAt(i.get()));
+        }
+        throw new IndexOutOfBoundsException("File ended abruptly");
+
+    }
+
+    private static void parseComment(String s, AtomicInteger i){
+        while (s.charAt(i.get()) != '\n'){
+            i.incrementAndGet();
+        }
+    }
+
+    private static void skipWhitespace(String s, AtomicInteger i){
+        while (Character.isWhitespace(s.charAt(i.get()))){
+            i.incrementAndGet();
+        }
+    }
+
+}

73a3a1569a7251adfb6593544250b9bf7ef945f8
Giuliano Rasper <s8girasp@stud.uni-saarland.de>
2020-01-19 22:53:50 +0100

    The communication module now supports sending much larger files - the Connection interface and it's implementation has been updated to be able to sent bytes in additional ways, which are ByteBuf's and File's

--- implementation/src/main/java/communication/wrapper/Connection.java
+++ implementation/src/main/java/communication/wrapper/Connection.java
@@ -1,8 +1,20 @@
 package communication.wrapper;
 
+import io.netty.buffer.ByteBuf;
+
+import java.io.File;
+
 public interface Connection {
 
     void send(String message);
+
+    void sendBytes(byte[] bytes);
+
+    void sendFile(File file);
+
+    void sendFile(File file, String name);
+
+    void sendBytes(byte[] bytes, String name);
 
     void close();
 }
--- implementation/src/main/java/communication/wrapper/NettyConnectionWrapper.java
+++ implementation/src/main/java/communication/wrapper/NettyConnectionWrapper.java
@@ -1,7 +1,18 @@
 package communication.wrapper;
 
+import communication.packets.response.DownloadFileResponsePacket;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class NettyConnectionWrapper implements Connection {
 
@@ -13,7 +24,53 @@ public class NettyConnectionWrapper implements Connection {
 
     @Override
     public void send(String message) {
-        context.channel().writeAndFlush(new TextWebSocketFrame(message));
+        context.writeAndFlush(new TextWebSocketFrame(message));
+    }
+
+    private void sendBytes(ByteBuf byteBuf) {
+        context.writeAndFlush(new BinaryWebSocketFrame(byteBuf));
+    }
+
+    @Override
+    public void sendBytes(byte[] bytes) {
+        ByteBuf byteBuf = Unpooled.wrappedBuffer(bytes);
+        System.out.println(byteBuf.readableBytes());
+        sendBytes(byteBuf);
+    }
+
+    @Override
+    public void sendFile(File file) {
+        sendFile(file, file.getName());
+    }
+
+    @Override
+    public void sendFile(File file, String name) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            FileChannel fileChannel = fileInputStream.getChannel();
+            MappedByteBuffer mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, file.length());
+            ByteBuf byteBuf = Unpooled.wrappedBuffer(mappedByteBuffer);
+            DownloadFileResponsePacket packet = new DownloadFileResponsePacket(null, name);
+            ChannelFuture future = context.writeAndFlush(new TextWebSocketFrame(packet.toJson()));
+            future.addListener((future1 -> {
+                if(future.isDone() && future.isSuccess()) sendBytes(byteBuf);
+            }));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void sendBytes(byte[] file, String name) {
+        try {
+            DownloadFileResponsePacket packet = new DownloadFileResponsePacket(null, name);
+            ChannelFuture future = context.writeAndFlush(new TextWebSocketFrame(packet.toJson()));
+            future.addListener((future1 -> {
+                if(future.isDone() && future.isSuccess()) sendBytes(file);
+            }));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override

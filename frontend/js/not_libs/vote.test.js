3baad16976428d56ecd122a67252f444533d836a
Muhammad Kamran <muhammad.kamran@dfki.de>
2020-01-22 19:33:48 +0100

    Added tests for vote panel

--- frontend/js/not_libs/vote.test.js
+++ frontend/js/not_libs/vote.test.js
@@ -1,20 +1,21 @@
-import {sum} from './voteTestFile.js';
+// import {sum} from './voteTestFile.js';
 
 import puppeteer from 'puppeteer';
 
 // import { startVoteResult } from './voting.js';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
 
 describe('Vote Panel', () => {
-// test('Vote Created Successfully', async () => {
-//   jest.setTimeout(50000);
+
+// test('Create Vote', async () => {
+//   jest.setTimeout(30000);
 //   const browser = await puppeteer.launch({
-//     headless: false,
-//     slowMo: 100,
-//     args: ['--window-size=1366,768']
+//     // headless: false,
+//     // slowMo: 100,
+//     // args: ['--window-size=1366,768']
 //   })
 //   const page = await browser.newPage();
 //   await page.goto('http://localhost/CMT/', {waitUntil: 'domcontentloaded'});
@@ -27,17 +28,17 @@ describe('Vote Panel', () => {
 //     page.waitForNavigation(), // The promise resolves after navigation has finished
 //     page.click('button#submit-button'), // Clicking the link will indirectly cause a navigation
 //   ]);
-//   // await page.waitFor(2000);
+
 //   await page.goto('http://localhost/CMT/vote.html', {waitUntil: 'domcontentloaded'});
+//   await page.waitFor(2000);
 //   await page.click('button#createVote');
-//   // await page.waitFor(3000);
+
 //   await page.click('input#VoteText');
-//   await page.type('input#VoteText', 'this is question 3');
+//   await page.type('input#VoteText', 'Vote Question');
 //   await page.click('input#duration');
 //   await page.type('input#duration', '1');
-//   // await page.evaluate(() => {
-//   //   return ('input[type=radio][value=0]').prop('checked', true);
-//   // });
+
+//   // By default vote is Named vote but here i make it anonymous i.e 0 value
 //   await page.$eval('input[type="radio"][value="0"]', radios => {
 //     // checkboxes.forEach(chbox => chbox.click())
 //     radios.click()
@@ -46,30 +47,11 @@ describe('Vote Panel', () => {
 
 //   await page.click('button#confirm');
 //   await page.waitFor(3000);
-//   const confirmCreateVote = await page.waitForXPath("//td[contains(., 'this is question 3')]").then(selector => {return "this is question 3"});
-//   // const confirmCreateVote = await page.$$eval('table tr td', tds => tds.map((td) => {
-//   //   if(td.innerHTML == 'this is question 22'){
-//   //     return td.innerHTML;
-//   //   }
-    
-//   // }));
+//   const confirmCreateVote = await page.waitForXPath("//td[contains(., 'Vote Question')]").then(selector => {return "Vote Question"});
 
-//   // const confirmCreateVote = await page.$eval()
-//   // const confirmCreateVote = await page.$$eval('table tr td', tds => tds.map((td) => {
-//   //   return td.innerHTML;
-//   // }));
-//   page.waitForXPath("//td[contains(., 'this is question 3')]").then(selector => selector.click());
+//   page.waitForXPath("//td[contains(., 'Vote Question')]").then(selector => selector.click());
 //   await page.waitFor(2000);
-//   // page.waitForXPath("//td[contains(., 'this is question 2')]").then(selector => selector.click());
-//   // const [button] = await page.$x("//button[contains(., 'Add')]");
-//   // if (button) {
-//   //     await button.click();
-//   // }
-//   // await page.waitFor(3000);
-//   console.log(confirmCreateVote);
-//     // const allButtons = await page.$$eval('input[type="button"]', el => {
-//     //   el.forEach(el1 => {return el1.innerText})
-//     // });
+//   // console.log(confirmCreateVote);
 //     var startVote;
 //     var addVote;
 //     var deleteVote;
@@ -96,7 +78,9 @@ describe('Vote Panel', () => {
 //     expect(deleteVote).toBe('Delete');
 //     expect(saveChanges).toBe('Save Changes');
 
-//   expect(confirmCreateVote).toBe('this is question 3');
+//   expect(confirmCreateVote).toBe('Vote Question');
+//   await page.waitFor(2000);
+//   console.log('Vote Created Successfully');
 //   // await browser.close();
 // });
 // -------------------------------------------Start Vote Test----------------------------------------
@@ -104,11 +88,11 @@ describe('Vote Panel', () => {
 
 // test('Start a vote', async () => {
 
-//   jest.setTimeout(50000);
+//   jest.setTimeout(30000);
 //   const browser = await puppeteer.launch({
-//     headless: false,
-//     slowMo: 100,
-//     args: ['--window-size=1366,768']
+//     // headless: false,
+//     // slowMo: 100,
+//     // args: ['--window-size=1366,768']
 //   })
 //   const page = await browser.newPage();
 //   await page.goto('http://localhost/CMT/', {waitUntil: 'domcontentloaded'});
@@ -123,8 +107,8 @@ describe('Vote Panel', () => {
 //   ]);
 //   // await page.waitFor(2000);
 //   await page.goto('http://localhost/CMT/vote.html', {waitUntil: 'domcontentloaded'});
-
-//   page.waitForXPath("//td[contains(., 'this is question')]").then(selector => selector.click());
+//   await page.waitFor(2000);
+//   page.waitForXPath("//td[contains(., 'Vote Question')]").then(selector => selector.click());
 //   await page.waitFor(2000);
 
 //   const [button] = await page.$x("//button[contains(., 'Add')]");
@@ -147,7 +131,7 @@ describe('Vote Panel', () => {
 //       await button1.click();
 //   }
 //   await page.waitFor(3000);
-//   console.log('Vote Has been started!');
+//   console.log('Vote has been started!');
 // });
 
 // -------------------------------------------------Delete Vote Test--------------------------
@@ -156,9 +140,9 @@ describe('Vote Panel', () => {
 // test("Delete Vote Test", async () => {
 //   jest.setTimeout(30000);
 //   const browser = await puppeteer.launch({
-//     headless: false,
-//     slowMo: 100,
-//     args: ['--window-size=1366,768']
+//     // headless: false,
+//     // slowMo: 100,
+//     // args: ['--window-size=1366,768']
 //   })
 //   const page = await browser.newPage();
 //   await page.goto('http://localhost/CMT/', {waitUntil: 'domcontentloaded'});
@@ -174,7 +158,7 @@ describe('Vote Panel', () => {
 //   // await page.waitFor(2000);
 //   await page.goto('http://localhost/CMT/vote.html', {waitUntil: 'domcontentloaded'});
 
-//   page.waitForXPath("//td[contains(., 'this is question 3')]").then(selector => selector.click());
+//   page.waitForXPath("//td[contains(., 'Vote Question')]").then(selector => selector.click());
 //   await page.waitFor(2000); 
 
 //   const [button1] = await page.$x("//button[contains(., 'Delete')]");
@@ -192,9 +176,9 @@ describe('Vote Panel', () => {
 
 //   jest.setTimeout(30000);
 //   const browser = await puppeteer.launch({
-//     headless: false,
-//     slowMo: 100,
-//     args: ['--window-size=1366,768']
+//     // headless: false,
+//     // slowMo: 100,
+//     // args: ['--window-size=1366,768']
 //   })
 //   const page = await browser.newPage();
 //   await page.goto('http://localhost/CMT/', {waitUntil: 'domcontentloaded'});
@@ -210,7 +194,7 @@ describe('Vote Panel', () => {
 //   // await page.waitFor(2000);
 //   await page.goto('http://localhost/CMT/vote.html', {waitUntil: 'domcontentloaded'});
 
-//   page.waitForXPath("//td[contains(., 'this is question 3')]").then(selector => selector.click());
+//   page.waitForXPath("//td[contains(., 'Vote Question')]").then(selector => selector.click());
 //   await page.waitFor(2000); 
 
 //   const [button] = await page.$x("//button[contains(., 'Add')]");
@@ -234,7 +218,6 @@ describe('Vote Panel', () => {
 //       await button1.click();
 //   }
 
-//   await page.evaluate(`window.confirm = () => true`);
 //   await page.waitFor(3000);
 //   console.log('Options saved successfully!');
 
@@ -242,12 +225,12 @@ describe('Vote Panel', () => {
 // --------------------------------------Submit answer for vote qeustion test ------------------------------
 // -------------------------------------------------------------------------------------------------
 
-// test('Vote submitted successfully', async () => {
+// test('Vote submission', async () => {
 //   jest.setTimeout(30000);
 //   const browser = await puppeteer.launch({
-//     headless: false,
-//     slowMo: 100,
-//     args: ['--window-size=1366,768']
+//     // headless: false,
+//     // slowMo: 100,
+//     // args: ['--window-size=1366,768']
 //   })
 //   const page = await browser.newPage();
 //   await page.goto('http://localhost/CMT/', {waitUntil: 'domcontentloaded'});
@@ -265,12 +248,11 @@ describe('Vote Panel', () => {
 //   await page.waitFor(3000);
 //   let voteQuestion = await page.$eval('span.d-flex', el => el.innerText );
 //   console.log(voteQuestion);
-//   expect(voteQuestion).toBe('newt');
+//   expect(voteQuestion).toBe('Vote Question New');
 //   // page.waitForXPath("//td[contains(., 'this is question 3')]").then(selector => selector.click());
 //   await page.waitFor(2000);
 
 //   await page.$eval('input[type="radio"]', radios => {
-//                                                                    // checkboxes.forEach(chbox => chbox.click())
 //     radios.click()
 //  });
 //  await page.waitFor(2000);
@@ -279,7 +261,7 @@ describe('Vote Panel', () => {
 //       await button1.click();
 //   } 
 //   await page.waitFor(2000);
-//   console.log("Vote submitted successfully");
+//   console.log("Vote submitted successfully!");
 
 // });
 
@@ -316,7 +298,82 @@ describe('Vote Panel', () => {
 // ----------------------------Remove Particular vote option---------------------
 // ------------------------------------------------------------------------------
 
-test('remove vote option', async () => {
+// test('Remove vote option', async () => {
+//   jest.setTimeout(30000);
+//   const browser = await puppeteer.launch({
+//     // headless: false,
+//     // slowMo: 100,
+//     // args: ['--window-size=1366,768']
+//   })
+//   const page = await browser.newPage();
+//   await page.goto('http://localhost/CMT/', {waitUntil: 'domcontentloaded'});
+
+//   await page.click('input#name');
+//   await page.type('input#name', 'admin');
+//   await page.click('input#password');
+//   await page.type('input#password', 'admin');
+//   await Promise.all([
+//     page.waitForNavigation(), // The promise resolves after navigation has finished
+//     page.click('button#submit-button'), // Clicking the link will indirectly cause a navigation
+//   ]);
+//   // await page.waitFor(2000);
+//   await page.goto('http://localhost/CMT/vote.html', {waitUntil: 'domcontentloaded'});
+
+//   page.waitForXPath("//td[contains(., 'Vote Question')]").then(selector => selector.click());
+//   await page.waitFor(2000); 
+//     const [button] = await page.$x("//button[contains(., 'Remove')]");
+//   if (button) {
+//       await button.click();
+//   }
+
+//   const [button1] = await page.$x("//button[contains(., 'Save Changes')]");
+//   if (button1) {
+//       await button1.click();
+//   }
+//   console.log('Option removed successfuly');
+
+// });
+
+// ---------------------------------------------Display Previous Votes---------------------------
+// ----------------------------------------------------------------------------------------------
+
+// test('Display Previous Votes', async () => {
+//   jest.setTimeout(30000);
+//   const browser = await puppeteer.launch({
+//     // headless: false,
+//     // slowMo: 100,
+//     // args: ['--window-size=1366,768']
+//   })
+//   const page = await browser.newPage();
+//   await page.goto('http://localhost/CMT/', {waitUntil: 'domcontentloaded'});
+
+//   await page.click('input#name');
+//   await page.type('input#name', 'admin');
+//   await page.click('input#password');
+//   await page.type('input#password', 'admin');
+//   await Promise.all([
+//     page.waitForNavigation(), // The promise resolves after navigation has finished
+//     page.click('button#submit-button'), // Clicking the link will indirectly cause a navigation
+//   ]);
+//   // await page.waitFor(2000);
+//   await page.goto('http://localhost/CMT/vote.html', {waitUntil: 'domcontentloaded'});
+//   await page.waitFor(3000);
+//   let voteQuestion = await page.$eval('h5.mb-0', el => el.innerText );
+  
+//   const [button1] = await page.$x("//button[contains(., 'see full details')]");
+//   if (button1) {
+//       await button1.click();
+//   }
+//   await page.waitFor(3000);
+//   expect(voteQuestion).toBeDefined();
+//   console.log(voteQuestion);
+  
+// });
+
+// ------------------------------create input text field option with button test------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+test('Create input text field with button test', async () => {
   jest.setTimeout(30000);
   const browser = await puppeteer.launch({
     // headless: false,
@@ -336,20 +393,34 @@ test('remove vote option', async () => {
   ]);
   // await page.waitFor(2000);
   await page.goto('http://localhost/CMT/vote.html', {waitUntil: 'domcontentloaded'});
+  await page.waitFor(3000);
 
-  page.waitForXPath("//td[contains(., 'this is question 3')]").then(selector => selector.click());
+  
+  page.waitForXPath("//td[contains(., 'Vote Question')]").then(selector => selector.click());
   await page.waitFor(2000); 
-    const [button] = await page.$x("//button[contains(., 'Remove')]");
-  if (button) {
-      await button.click();
-  }
 
+  const [button] = await page.$x("//button[contains(., 'Add')]");
+  if (button) {
+    
+    for(var num = 3; num >= 1; num--){
+      await button.click();
+    }
+  }
+     
+    await page.$$eval('input[type=text]', el => {
+      var i = 0;   
+    el.forEach(el1 => {
+      el1.value = 'testing'+i;
+      i++;})
+  });
+
+  await page.waitFor(2000); 
   const [button1] = await page.$x("//button[contains(., 'Save Changes')]");
   if (button1) {
       await button1.click();
   }
-  console.log('Removed successfuly');
-
+  console.log("Input fields created successfully!");
+  
 });
 
 });
